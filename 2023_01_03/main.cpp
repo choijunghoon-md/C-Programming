5ca2c1946455b19c45df7efb594bf325b8bf1ae6
CHOI_JUNG_HOON <118896663+choijunghoon-md@users.noreply.github.com>
2023-01-03 09:06:40 +0900

    Add files via upload

---
+++ 2023_01_03/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define NO_MAX_WAYPOINTS 50
+float   X[NO_MAX_WAYPOINTS] = { 0, };
+float   Y[NO_MAX_WAYPOINTS] = { 0, };
+float Yaw[NO_MAX_WAYPOINTS] = { 0, };
+float result;
+
+int read_waypoints_data(void);
+
+int main(void){
+
+	read_waypoints_data();
+
+	return 1;
+}
+
+int read_waypoints_data(void){
+	FILE *fp;
+	int no_line = 0;
+	float x, y, yaw;
+	fp = fopen("waypoints.dat", "rt");
+	do{
+		result = fscanf(fp, "%f %f %f", &x, &y, &yaw);
+		X[no_line] = x;
+		Y[no_line] = y;
+		Yaw[no_line] = yaw;
+		no_line++;
+	} while (feof(fp) == 0);
+	for (int i = 0; i < no_line; i++){
+		printf("\n %f %f %f \n", X[i], Y[i], Yaw[i]);
+	}
+	printf("no of line %d\n", no_line);
+
+	return 0;
+}

03da1b2a78f0232c1b93547caf9f278941f0542f
CHOI_JUNG_HOON <118896663+choijunghoon-md@users.noreply.github.com>
2023-01-10 13:19:40 +0900

    Add files via upload

---
+++ 2023_01_10/예제 6,7/main.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int N,K;
+	
+	scanf("%d %d",&N,&K);
+	
+	if((N>=2&&N<=100000)&&(K>=2&&K<=100000)){
+		N -= K*(K+1)/2;
+		if(N<0){printf("-1\n");}
+		else {printf("%d\n",K-1+(N%K != 0));}
+	}
+	
+	
+	return 0;
+}
